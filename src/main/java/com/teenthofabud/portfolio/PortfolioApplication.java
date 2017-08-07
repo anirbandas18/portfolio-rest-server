24d35802f6c9296863760498ab537dcfd253dce7
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-08-07 23:18:45 +0530

    initial commit

---
+++ src/main/java/com/teenthofabud/portfolio/PortfolioApplication.java
@@ -0,0 +1,14 @@
+package com.teenthofabud.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+}
