f92eeddf233a7025688c288c10204f0c9bcd5a94
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-08-15 03:34:50 +0530

    added rest api documentation

--- src/main/java/com/teenthofabud/portfolio/configuration/Swagger2Configuration.java
+++ src/main/java/com/teenthofabud/portfolio/configuration/Swagger2Configuration.java
@@ -2,17 +2,24 @@ package com.teenthofabud.portfolio.configuration;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfiguration {
+public class Swagger2Configuration {
 	
 	@Bean
     public Docket freelancerApi() {
@@ -20,7 +27,14 @@ public class SwaggerConfiguration {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.teenthofabud.portfolio.controller"))
                 .paths(regex("/freelancer.*"))
-                .build();
+                .build().apiInfo(metaData());
+    }
+	
+	private ApiInfo metaData() {
+		Collection<VendorExtension> vendors = new ArrayList<>();
+		Contact contact = new Contact("Anirban Das", "http://github.com/anirbandas18", "anirbandas18@live.com");
+        ApiInfo apiInfo = new ApiInfo("Portfolio API", "REST to CRUD mapping over HTTP on various portfolio models", "1.0", "Terms of service", contact, "Apache License Version 2.0", "https://www.apache.org/licenses/LICENSE-2.0", vendors);
+        return apiInfo;
     }
 
 }
--- src/main/java/com/teenthofabud/portfolio/controller/FreelancerController.java
+++ src/main/java/com/teenthofabud/portfolio/controller/FreelancerController.java
@@ -23,83 +23,100 @@ import com.teenthofabud.portfolio.service.ResumeService;
 import com.teenthofabud.portfolio.vo.FreelancerVO;
 import com.teenthofabud.portfolio.vo.ResumeVO;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+
 @RestController
 @RequestMapping("/freelancer")
+@Api(consumes = "application/json", produces = "application/json", protocols = "http", tags = {
+		"Freelancer" }, description = "REST to CRUD mapping on freelancer and it's resume")
 public class FreelancerController {
-	
+
 	@Autowired
 	private ResumeService resumeService;
-	
+
 	@Autowired
 	private FreelancerService freelancerService;
-	
+
+	@ApiOperation(value = "read freelancer details", response = Freelancer.class, produces = "application/json", 
+			notes = "Read details of freelancer from database as identified by its ID and expose it ")
 	@GetMapping("/{id}")
-	public ResponseEntity<Freelancer> getFreelancerDetails(@PathVariable String id) throws ServiceException {
+	public ResponseEntity<Freelancer> getFreelancerDetails(
+			@ApiParam(value = "freelancer ID", required = true) @PathVariable String id) throws ServiceException {
 		Freelancer freelancer = freelancerService.readFreelancer(id);
-		ResponseEntity<Freelancer> response = ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON)
+		ResponseEntity<Freelancer> response = ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON)
 				.body(freelancer);
 		return response;
 	}
-	
+
+	@ApiOperation(value = "delete freelancer details", response = Freelancer.class, produces = "application/json", 
+			notes = "Delete details of freelancer from database as identified by its ID and expose the operation status ")
 	@DeleteMapping("/{id}")
-	public ResponseEntity<FreelancerVO> deleteFreelancerDetails(@PathVariable String id) throws ServiceException {
+	public ResponseEntity<FreelancerVO> deleteFreelancerDetails(
+			@ApiParam(value = "freelancer ID", required = true) @PathVariable String id) throws ServiceException {
 		Boolean changed = freelancerService.deleteFreelancer(id);
 		FreelancerVO f = new FreelancerVO();
 		f.setId(id);
 		f.setChanged(changed);
-		ResponseEntity<FreelancerVO> response = ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(f);
+		ResponseEntity<FreelancerVO> response = ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(f);
 		return response;
 	}
 	
+	@ApiOperation(value = "create freelancer details", response = Freelancer.class, produces = "application/json", 
+			notes = "Create details of freelancer in database with corresponding data passed as JSON in request body ")
 	@PostMapping
-	public ResponseEntity<FreelancerVO> postFreelancerDetails(@RequestBody Freelancer freelancer) throws ServiceException {
+	public ResponseEntity<FreelancerVO> postFreelancerDetails(
+			@ApiParam(value = "freelancer details", required = true) @RequestBody Freelancer freelancer)
+			throws ServiceException {
 		String id = freelancerService.createFreelancer(freelancer);
 		String name = freelancer.getFirstName() + " " + freelancer.getLastName();
 		FreelancerVO f = new FreelancerVO();
 		f.setId(id);
 		f.setName(name);
-		ResponseEntity<FreelancerVO> response = ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(f);
+		ResponseEntity<FreelancerVO> response = ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(f);
 		return response;
 	}
-	
+
+	@ApiOperation(value = "update freelancer details", response = Freelancer.class, produces = "application/json", 
+			notes = "Update details of freelancer in database with corresponding data passed as JSON in request body ")
 	@PutMapping
-	public ResponseEntity<FreelancerVO> putFreelancerDetails(@RequestBody Freelancer freelancer) throws ServiceException {
+	public ResponseEntity<FreelancerVO> putFreelancerDetails(
+			@ApiParam(value = "freelancer details", required = true) @RequestBody Freelancer freelancer)
+			throws ServiceException {
 		Boolean changed = freelancerService.updateFreelancer(freelancer);
 		FreelancerVO f = new FreelancerVO();
 		f.setId(freelancer.getId());
 		f.setChanged(changed);
-		ResponseEntity<FreelancerVO> response = ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(f);
+		ResponseEntity<FreelancerVO> response = ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(f);
 		return response;
 	}
-	
+
+	@ApiOperation(value = "upload freelancer resume file", response = Freelancer.class, produces = "application/json", 
+			notes = "Upload resume file of freelancer as identified by its respective ID and store on the file system. Respond with checksum generated for the file alongwith it's size in bytes")
 	@PostMapping("/resume/{id}")
-	public ResponseEntity<ResumeVO> uploadResume(@RequestParam MultipartFile resume, @PathVariable String id) throws ServiceException {
+	public ResponseEntity<ResumeVO> uploadResume(
+			@ApiParam(value = "freelancer's resume file", required = true) @RequestParam MultipartFile resume, 
+			@ApiParam(value = "freelancer ID", required = true) @PathVariable String id)
+			throws ServiceException {
 		String md5 = resumeService.importResume(resume, id);
 		ResumeVO f = new ResumeVO();
 		f.setCheckSum(md5);
 		f.setSize(resume.getSize());
-		ResponseEntity<ResumeVO> response = ResponseEntity.ok()
-				.contentType(MediaType.APPLICATION_JSON)
-				.body(f);
+		ResponseEntity<ResumeVO> response = ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(f);
 		return response;
 	}
 	
+	@ApiOperation(value = "download freelancer resume file", response = Freelancer.class, produces = "application/json", 
+			notes = "Download resume file of freelancer as identified by its respective ID from the file system")
 	@GetMapping("/resume/{id}")
-	public ResponseEntity<Resource> downloadResume(@PathVariable String id) throws ServiceException {
+	public ResponseEntity<Resource> downloadResume(
+			@ApiParam(value = "freelancer ID", required = true) @PathVariable String id) throws ServiceException {
 		byte[] resume = resumeService.exportResume(id);
 		Resource resource = new ByteArrayResource(resume);
-		ResponseEntity<Resource> response = ResponseEntity.ok()
-				.contentLength(resume.length)
-				.contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE))
-				.body(resource);
+		ResponseEntity<Resource> response = ResponseEntity.ok().contentLength(resume.length)
+				.contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE)).body(resource);
 		return response;
 	}
-	
+
 }
