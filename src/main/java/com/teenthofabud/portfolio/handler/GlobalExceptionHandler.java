54c151d224b4950d67fb71e506bec2d7fb00f993
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-08-17 13:55:48 +0530

    added separation of concern by common exception policy for similar resources

--- src/main/java/com/teenthofabud/portfolio/handler/GlobalExceptionHandler.java
+++ src/main/java/com/teenthofabud/portfolio/handler/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.teenthofabud.portfolio.handler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,20 +11,30 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import com.teenthofabud.portfolio.exception.ServiceException;
 import com.teenthofabud.portfolio.service.UtilityServices;
 import com.teenthofabud.portfolio.vo.ErrorVO;
+import com.teenthofabud.portfolio.vo.ExceptionVO;
 
 @RestControllerAdvice
-public class GlobalRestExceptionHandler {
+public class GlobalExceptionHandler {
 	
 	@Autowired
 	private UtilityServices utilityServices;
 	
 	@ExceptionHandler(value = ServiceException.class)
-	public ResponseEntity<?> handleServiceException(ServiceException e) {
+	public ResponseEntity<?> handleControllerException(ServiceException e) {
 		String description = utilityServices.wrapServiceException(e);
 		ErrorVO body = new ErrorVO(e.getStatus().value(), description);
 		HttpHeaders headers = new HttpHeaders();
 		headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 		ResponseEntity<ErrorVO> response = new ResponseEntity<>(body, headers, e.getStatus());
+		return response;
+	}
+	
+	@ExceptionHandler(value = Exception.class)
+	public ResponseEntity<?> handleOtherException(Exception e) {
+		ExceptionVO body = new ExceptionVO(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getCause().getClass().getName(), e.getMessage());
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		ResponseEntity<ErrorVO> response = new ResponseEntity<>(body, headers, HttpStatus.INTERNAL_SERVER_ERROR);
 		return response;
 	}
 	
---
+++ src/main/java/com/teenthofabud/portfolio/vo/ExceptionVO.java
@@ -0,0 +1,20 @@
+package com.teenthofabud.portfolio.vo;
+
+public class ExceptionVO extends ErrorVO {
+	
+	private String cause;
+
+	public ExceptionVO(Integer code, String cause, String message) {
+		super(code, message);
+		this.cause = cause;
+	}
+
+	public String getCause() {
+		return cause;
+	}
+
+	public void setCause(String cause) {
+		this.cause = cause;
+	}
+
+}
