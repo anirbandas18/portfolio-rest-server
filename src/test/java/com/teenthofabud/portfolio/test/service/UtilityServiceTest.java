ff83c3e829cbf8426ca32a1827438a3077703665
anirban <anirban@PC-ANIRBAN.global.cadence.com>
2017-09-07 00:23:38 +0530

    utility service test for complete,partial,different,empty map

--- src/test/java/com/teenthofabud/portfolio/test/service/UtilityServiceTest.java
+++ src/test/java/com/teenthofabud/portfolio/test/service/UtilityServiceTest.java
@@ -10,12 +10,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.PathMatcher;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teenthofabud.portfolio.core.service.UtilityService;
 import com.teenthofabud.portfolio.test.model.POJO;
+import com.teenthofabud.portfolio.vo.ResponseVO;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE, classes = { PathMatcher.class, UtilityService.class, ObjectMapper.class })
@@ -24,32 +26,60 @@ public class UtilityServiceTest {
 	@Autowired
 	private UtilityService util;
 	
-	private Map<String,Object> map;
-	private POJO pojo;
+	private Map<String,Object> completeMap, partialMap, differentMap, emptyMap;
+	private POJO completePojo;
 	
 	@Before
 	public void init() {
 		Long now = System.currentTimeMillis();
 		
-		map = new LinkedHashMap<>();
-		map.put("id", 1);
-		map.put("salary", 76543245.78d);
-		map.put("name", "Emp1");
-		map.put("registration", now);
+		completeMap = new LinkedHashMap<>();
+		completeMap.put("id", 1);
+		completeMap.put("salary", 76543245.78d);
+		completeMap.put("name", "Emp1");
+		completeMap.put("registration", now);
 		
-		pojo = new POJO(1, 76543245.78d, "Emp1", now);
+		partialMap = new LinkedHashMap<>();
+		partialMap.put("id", 1);
+		partialMap.put("salary", 76543245.78d);
+		
+		differentMap = new LinkedHashMap<>();
+		differentMap.put("status", HttpStatus.OK);
+		differentMap.put("message", "Found");
+		
+		emptyMap = new LinkedHashMap<>();
+		
+		completePojo = new POJO(1, 76543245.78d, "Emp1", now);
 	}
 	
 	@Test
-	public void shouldReturnPOJOWhenMapIsGiven() {
-		Object o = util.map2POJO(map, POJO.class);
-		Assert.assertEquals(pojo, o);
+	public void shouldReturnCompletePOJOForCompleteMap() {
+		Object o = util.map2POJO(completeMap, POJO.class);
+		Assert.assertEquals(completePojo, o);
 	}
 	
 	@Test
-	public void shouldReturnMapWhenPOJOIsGiven() {
-		Map<String,Object> map = util.pojo2Map(pojo);
-		Assert.assertEquals(map, this.map);
+	public void shouldReturnCompleteMapForCompletePOJO() {
+		Map<String,Object> map = util.pojo2Map(completePojo);
+		Assert.assertEquals(map, completeMap);
+	}
+	
+	@Test
+	public void shouldNotMatchCompletePOJOForPartialMap() {
+		Object o = util.map2POJO(partialMap, POJO.class);
+		Assert.assertNotEquals(completePojo, o);
+	}
+	
+	@Test
+	public void shouldNotMatchCompletePOJOForDifferentMap() {
+		Object o = util.map2POJO(differentMap, ResponseVO.class);
+		Assert.assertNotEquals(completePojo, o);
 	}
 
+	@Test
+	public void shouldNotMatchCompletePOJOForEmptyMap() {
+		Object o = util.map2POJO(emptyMap, POJO.class);
+		Assert.assertNotEquals(completePojo, o);
+	}
+	
 }
